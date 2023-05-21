1af028b01d86bfe2b2f8659739d434c533c7689d
Mohamed Abosora <mabosora@uni-bremen.de>
2023-05-21 15:50:38 +0200

    Added 3 Fragments

--- app/src/main/java/com/example/runzone/MainActivity.kt
+++ app/src/main/java/com/example/runzone/MainActivity.kt
@@ -1,11 +1,50 @@
 package com.example.runzone
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.example.runzone.databinding.ActivityMainBinding
+import com.example.runzone.databinding.ActivitySignInBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val homeFragment = HomeFragment()
+        replaceFragment(homeFragment)
+
+        binding.bottomNavigationView.setOnItemSelectedListener {item ->
+            run {
+                when (item.itemId) {
+                    R.id.home_tab -> {
+                        val homeFragment = HomeFragment()
+                        replaceFragment(homeFragment)
+                    }
+                    R.id.profile_tab -> {
+                        val profileFragment = ProfileFragment()
+                        replaceFragment(profileFragment)
+                    }
+                    R.id.settings_tab -> {
+                        val settingsFragment = SettingsFragment()
+                        replaceFragment(settingsFragment)
+                    }
+                    else -> {}
+                }
+            }
+            return@setOnItemSelectedListener true
+        }
+
     }
+
+    fun replaceFragment(fragment: Fragment) {
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout, fragment)
+        fragmentTransaction.commit()
+    }
+
 }
