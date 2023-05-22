eaa5e697ce79b8c75e7501a187dc63c8623ab160
Mohamed Abosora <mabosora@uni-bremen.de>
2023-05-23 00:08:20 +0200

    show email in ProfileFragment

--- app/src/main/java/com/example/runzone/ProfileFragment.kt
+++ app/src/main/java/com/example/runzone/ProfileFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import com.google.firebase.auth.FirebaseAuth
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -21,6 +23,9 @@ class ProfileFragment : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
 
+    val firebaseAuth = FirebaseAuth.getInstance()
+    val currentUser = firebaseAuth.currentUser
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -34,7 +39,15 @@ class ProfileFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+
+        val view = inflater.inflate(R.layout.fragment_profile, container, false)
+        val textViewEmail = view.findViewById<TextView>(R.id.textViewEmail)
+        if (currentUser != null) {
+            val email = currentUser.email
+            textViewEmail.text = "Email : $email"
+        }
+
+        return view
     }
 
     companion object {
