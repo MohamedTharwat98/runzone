a5c2d58dcbcf952fc71086e2d8bc4a1b98fb4eab
Mohamed Abosora <mohamed.abosora@hmmh.de>
2023-08-20 20:27:30 +0200

    Child classes for every Mission 2

---
+++ app/src/main/java/com/example/runzone/DirectFeedbackMission.kt
@@ -0,0 +1,52 @@
+package com.example.runzone
+
+import android.os.Build
+import android.os.Bundle
+import androidx.annotation.RequiresApi
+
+class DirectFeedbackMission : HeartRateActivity (){
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Add your custom code here for ChildActivity1's onCreate method
+    }
+
+    // Override the timerRunnable here
+    private val customTimerRunnable = object : Runnable {
+        @RequiresApi(Build.VERSION_CODES.O)
+        override fun run() {
+            // Custom implementation for timerRunnable
+            // ...
+            seconds++
+            val hours = seconds / 3600
+            val minutes = (seconds % 3600) / 60
+            val secs = seconds % 60
+
+            timerTextView.text = String.format("%02d:%02d:%02d", hours, minutes, secs)
+            timerTextView.text = " ${timerTextView.text} \n Time"
+            handler.postDelayed(this, 1000)
+
+            checkAudio(minutes,secs)
+
+            readAllData()
+        }
+    }
+
+
+
+    private fun checkAudio (minutes: Int,  secs : Int) {
+
+    }
+
+
+    override fun startTimer() {
+        isRunning = true
+        handler.postDelayed(customTimerRunnable, 1000)
+    }
+
+    override fun stopTimer() {
+        isRunning = false
+        handler.removeCallbacks(customTimerRunnable)
+    }
+
+}
---
+++ app/src/main/java/com/example/runzone/EscapeFromDystopia.kt
@@ -0,0 +1,58 @@
+package com.example.runzone
+
+import android.media.MediaPlayer
+import android.os.Build
+import android.os.Bundle
+import androidx.annotation.RequiresApi
+
+class EscapeFromDystopia : HeartRateActivity (){
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Add your custom code here for ChildActivity1's onCreate method
+    }
+
+    // Override the timerRunnable here
+    private val customTimerRunnable = object : Runnable {
+        @RequiresApi(Build.VERSION_CODES.O)
+        override fun run() {
+            // Custom implementation for timerRunnable
+            // ...
+            seconds++
+            val hours = seconds / 3600
+            val minutes = (seconds % 3600) / 60
+            val secs = seconds % 60
+
+            timerTextView.text = String.format("%02d:%02d:%02d", hours, minutes, secs)
+            timerTextView.text = " ${timerTextView.text} \n Time"
+            handler.postDelayed(this, 1000)
+
+            checkAudio(minutes,secs)
+
+            readAllData()
+        }
+    }
+
+    private fun checkAudio (minutes: Int,  secs : Int) {
+        if (minutes == 0 && secs == 5) {
+            val mediaPlayer = MediaPlayer.create(this@EscapeFromDystopia, R.raw.narrator1)
+            mediaPlayer.start()
+        }
+        if (minutes == 0 && secs == 35) {
+            val mediaPlayer = MediaPlayer.create(this@EscapeFromDystopia, R.raw.resistanceleader)
+            mediaPlayer.start()
+        }
+    }
+
+
+
+    override fun startTimer() {
+        isRunning = true
+        handler.postDelayed(customTimerRunnable, 1000)
+    }
+
+    override fun stopTimer() {
+        isRunning = false
+        handler.removeCallbacks(customTimerRunnable)
+    }
+}
